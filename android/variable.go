d609598450a9b1eab37882019cb8706857d1cfb6
klozz <shaclone707@gmail.com>
2018-08-17 07:40:51 -0400

    Revert "soong: Add needs_text_relocations product variable"
    This reverts commit 6f61003611ac5276cca32f606b907fd6797845f3.
    
    Change-Id: I6683a364f22d0802f027888f97a22ee6946d155d
    Signed-off-by: klozz <shaclone707@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -124,10 +124,6 @@ type variableProperties struct {
 		Device_support_hwfde_perf struct {
 			Cflags []string
 		}
-
-		Needs_text_relocations struct {
-			Cppflags []string
-		}
 	} `android:"arch_variant"`
 }
 
@@ -246,8 +242,6 @@ type productVariables struct {
 	PgoAdditionalProfileDirs []string `json:",omitempty"`
 
 	VendorVars map[string]map[string]string `json:",omitempty"`
-
-	Needs_text_relocations *bool `json:",omitempty"`
 }
 
 func boolPtr(v bool) *bool {
