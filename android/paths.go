a0805eb511384a5249414ab791f4726978118a1f
klozz <shaclone707@gmail.com>
2018-12-05 18:55:38 -0500

    add path for source relaxed from Lineage os Reference commit https://github.com/LineageOS/android_build_soong/commit/46c841786b5535782e44dafa54e8736c4805e2dd
    Change-Id: I2bb7b90e0b41a29da3771aa7dd35662092242792
    Signed-off-by: klozz <shaclone707@gmail.com>

--- android/paths.go
+++ android/paths.go
@@ -534,6 +534,32 @@ func pathForSource(ctx PathContext, pathComponents ...string) (SourcePath, error
 
 	return ret, nil
 }
+// pathForSourceRelaxed creates a SourcePath from pathComponents, but does not check that it exists.
+// It differs from pathForSource in that the path is allowed to exist outside of the PathContext.
+func pathForSourceRelaxed(ctx PathContext, pathComponents ...string) (SourcePath, error) {
+	p := filepath.Join(pathComponents...)
+	ret := SourcePath{basePath{p, ctx.Config(), ""}}
+
+ 	abs, err := filepath.Abs(ret.String())
+	if err != nil {
+		return ret, err
+	}
+
+	buildroot, err := filepath.Abs(ctx.Config().buildDir)
+	if err != nil {
+		return ret, err
+	}
+
+	if strings.HasPrefix(abs, buildroot) {
+		return ret, fmt.Errorf("source path %s is in output", abs)
+	}
+
+ 	if pathtools.IsGlob(ret.String()) {
+		return ret, fmt.Errorf("path may not contain a glob: %s", ret.String())
+	}
+
+ 	return ret, nil
+}
 
 // existsWithDependencies returns true if the path exists, and adds appropriate dependencies to rerun if the
 // path does not exist.
@@ -583,6 +609,34 @@ func PathForSource(ctx PathContext, pathComponents ...string) SourcePath {
 	}
 	return path
 }
+
+// PathForSourceRelaxed joins the provided path components.  Unlike PathForSource,
+// the result is allowed to exist outside of the source dir.
+// On error, it will return a usable, but invalid SourcePath, and report a ModuleError.
+func PathForSourceRelaxed(ctx PathContext, pathComponents ...string) SourcePath {
+	path, err := pathForSourceRelaxed(ctx, pathComponents...)
+
+	if err != nil {
+		reportPathError(ctx, err)
+	}
+
+ 	if modCtx, ok := ctx.(ModuleContext); ok && ctx.Config().AllowMissingDependencies() {
+		exists, err := existsWithDependencies(ctx, path)
+		if err != nil {
+			reportPathError(ctx, err)
+		}
+		if !exists {
+			modCtx.AddMissingDependencies([]string{path.String()})
+		}
+
+	} else if exists, _, err := ctx.Fs().Exists(path.String()); err != nil {
+		reportPathErrorf(ctx, "%s: %s", path, err.Error())
+	} else if !exists {
+		reportPathErrorf(ctx, "source path %s does not exist", path)
+	}
+	return path
+}
+
 
 // ExistentPathForSource returns an OptionalPath with the SourcePath if the
 // path exists, or an empty OptionalPath if it doesn't exist. Dependencies are added
