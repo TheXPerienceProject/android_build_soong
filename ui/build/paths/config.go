55b6d19263e1a9160bf8f5ee25b6738fe625bb30
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-07 23:13:38 -0300

    soong: Add 'repo' to allowed commands
    Change-Id: I49927cf3c3b08bd9e744dfbd88e6269681c8057d
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -92,6 +92,7 @@ var Configuration = map[string]PathConfig{
 	"openssl": Allowed,
 	"pstree":  Allowed,
 	"rsync":   Allowed,
+        "repo":    Allowed,
 	"sh":      Allowed,
 	"tr":      Allowed,
 	"unzip":   Allowed,
