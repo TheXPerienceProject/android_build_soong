e6c7977adddce9ae59352fe403d1f07f1de4d90f
Sam Mortimer <sam@mortimer.me.uk>
2019-10-07 11:41:14 -0700

    soong: Give priority to modules in exported namespaces for bootjars
    * Looking for modules that provide a boot jar is currently
      done by module name match only.  This breaks when you
      have multiple copies of the same module in your build
      tree (eg in another device).
    
    * Add a new mutator that marks the modules that should be used
      for bootjar content.
    
    * The logic is simple: give priority to modules that are in
      exported soong namespace.
    
    Change-Id: I2476174b892475c14a9f10b5e66b8496186f81c0

--- android/module.go
+++ android/module.go
@@ -538,6 +538,9 @@ type commonProperties struct {
 
 	// set by ImageMutator
 	ImageVariation string `blueprint:"mutated"`
+
+	// Whether this module provides a boot jar
+	BootJarProvider bool `blueprint:"mutated"`
 }
 
 type hostAndDeviceProperties struct {
@@ -801,6 +804,10 @@ func (m *ModuleBase) String() string {
 	}
 	sb.WriteString("}")
 	return sb.String()
+}
+
+func (a *ModuleBase) BootJarProvider() bool {
+	return a.commonProperties.BootJarProvider
 }
 
 // BaseModuleName returns the name of the module as specified in the blueprints file.
