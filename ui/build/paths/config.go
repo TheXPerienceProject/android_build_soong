b848f8b96fbb308eb4dbf6e225cbd33e3b9485c5
Anay Wadhera <awadhera@berkeley.edu>
2020-05-29 23:42:11 -0700

    soong: whitelist ccache
    Change-Id: I68e5321804c8b1b4a6fcd4690de197440bef6ea6
    Signed-off-by: klozz <carlosj@klozz.dev>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -82,6 +82,7 @@ var Configuration = map[string]PathConfig{
 	"aarch64-linux-android-objdump": Log,
 	"bash":    Allowed,
         "brotli":  Allowed,
+        "ccache":  Allowed,
 	"cpio":    Allowed,
 	"curl":    Allowed,
 	"date":    Allowed,
