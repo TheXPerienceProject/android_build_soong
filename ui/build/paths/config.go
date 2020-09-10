41b2fca6d54ffd04b6d31714c9d710e2366ebdaf
Lucchetto <lucchetto.tie@live.com>
2020-09-10 06:24:14 +0200

    soong: whitelist brotli
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: Iae3c1360a4694cd988d89d401ef18c75320c2abb
    Signed-off-by: klozz <carlosj@klozz.dev>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -81,6 +81,7 @@ var Configuration = map[string]PathConfig{
 	"aarch64-linux-android-objcopy": Log,
 	"aarch64-linux-android-objdump": Log,
 	"bash":    Allowed,
+        "brotli":  Allowed,
 	"cpio":    Allowed,
 	"curl":    Allowed,
 	"date":    Allowed,
