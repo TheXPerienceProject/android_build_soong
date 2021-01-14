8aa1b8ec578fe00e420820806a2fee26da993d5b
Ryan Prichard <rprichard@google.com>
2021-01-13 19:18:53 -0800

    Switch platform EH unwinder to prebuilt LLVM libunwind
    For the platform libc++ STL, remove the
    -Wl,--exclude-libs,libunwind_llvm.a argument, which is redundant with
    the same argument in deviceGlobalLdflags.
    
    Bug: http://b/153025717
    Test: device boots
    Change-Id: Idd7791d52f74aab2d5f59419fb75f841fc29a2eb
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- apex/apex_test.go
+++ apex/apex_test.go
@@ -1392,11 +1392,11 @@ func TestQTargetApexUsesStaticUnwinder(t *testing.T) {
 	`)
 
 	// ensure apex variant of c++ is linked with static unwinder
-	cm := ctx.ModuleForTests("libc++", "android_arm64_armv8-a_shared_apex29").Module().(*cc.Module)
-	ensureListContains(t, cm.Properties.AndroidMkStaticLibs, "libgcc_stripped")
+	cm := ctx.ModuleForTests("libc++", "android_arm64_armv8-a_shared_myapex").Module().(*cc.Module)
+	ensureListContains(t, cm.Properties.AndroidMkStaticLibs, "libunwind")
 	// note that platform variant is not.
 	cm = ctx.ModuleForTests("libc++", "android_arm64_armv8-a_shared").Module().(*cc.Module)
-	ensureListNotContains(t, cm.Properties.AndroidMkStaticLibs, "libgcc_stripped")
+	ensureListNotContains(t, cm.Properties.AndroidMkStaticLibs, "libunwind")
 }
 
 func TestInvalidMinSdkVersion(t *testing.T) {
