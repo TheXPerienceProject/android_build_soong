b23338e1809c2e667b99ca9e3cee02eb7a66e052
Jarl-Penguin <jarlpenguin@outlook.com>
2021-07-20 15:31:28 +0300

    cleanbuild: Add `recovery_kernel` to installclean files
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I89b7093be92cb22a223033f9a7269624f7293c35

--- ui/build/cleanbuild.go
+++ ui/build/cleanbuild.go
@@ -137,6 +137,7 @@ func installClean(ctx Context, config Config) {
 		productOut("build_fingerprint.txt"),
 		productOut("kernel"),
 		productOut("kernel-*"),
+		productOut("recovery_kernel"),
 		productOut("data"),
 		productOut("skin"),
 		productOut("obj/DTB_OBJ"),
