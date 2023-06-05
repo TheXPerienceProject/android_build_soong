24f2eeb4318d4e4ce27b856e26b808df66d1acfc
Jens Reidel <adrian@travitia.xyz>
2023-06-05 15:52:12 +0300

    soong: Add armv9-a aarch32 variant and cortex-a710 CPU
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

--- cc/config/arm64_device.go
+++ cc/config/arm64_device.go
@@ -44,7 +44,7 @@ var (
 		// On ARMv9 and later, Pointer Authentication Codes (PAC) are mandatory,
 		// so -fstack-protector is unnecessary.
 		"armv9-a": []string{
-			"-march=armv9-a+nosve",
+			"-march=armv9-a+nosve+dotprod",
 			"-mbranch-protection=standard",
 		},
 		"armv9-2a": []string{
@@ -72,6 +72,9 @@ var (
 		},
 		"cortex-a55": []string{
 			"-mcpu=cortex-a55",
+		},
+		"cortex-a710": []string{
+			"-mcpu=cortex-a710",
 		},
 		"cortex-a75": []string{
 			// Use the cortex-a55 since it is similar to the little
@@ -127,6 +130,7 @@ func init() {
 	pctx.StaticVariable("Arm64CortexA510Cflags", strings.Join(arm64CpuVariantCflags["cortex-a510"], " "))
 	pctx.StaticVariable("Arm64CortexA53Cflags", strings.Join(arm64CpuVariantCflags["cortex-a53"], " "))
 	pctx.StaticVariable("Arm64CortexA55Cflags", strings.Join(arm64CpuVariantCflags["cortex-a55"], " "))
+	pctx.StaticVariable("Arm64CortexA710Cflags", strings.Join(arm64CpuVariantCflags["cortex-a710"], " "))
 	pctx.StaticVariable("Arm64KryoCflags", strings.Join(arm64CpuVariantCflags["kryo"], " "))
 	pctx.StaticVariable("Arm64ExynosM1Cflags", strings.Join(arm64CpuVariantCflags["exynos-m1"], " "))
 	pctx.StaticVariable("Arm64ExynosM2Cflags", strings.Join(arm64CpuVariantCflags["exynos-m2"], " "))
@@ -139,6 +143,7 @@ var (
                 "cortex-a510": "${config.Arm64CortexA510Cflags}",
 		"cortex-a53": "${config.Arm64CortexA53Cflags}",
 		"cortex-a55": "${config.Arm64CortexA55Cflags}",
+		"cortex-a710": "${config.Arm64CortexA710Cflags}",
 		"cortex-a72": "${config.Arm64CortexA53Cflags}",
 		"cortex-a73": "${config.Arm64CortexA53Cflags}",
 		"cortex-a75": "${config.Arm64CortexA55Cflags}",
