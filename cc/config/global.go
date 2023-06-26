aa1876d74919d1eb4bcbb9a657aca898c9da3912
zijunzhao <zijunzhao@google.com>
2023-06-26 21:41:25 +0000

    Enable -Wenum-constexpr-conversion globally
    Bug: b/243964282
    Test: make checkbuild
    Change-Id: I187856ebb77c0634b43908b63d5cfbe2ca747158

--- cc/config/global.go
+++ cc/config/global.go
@@ -288,8 +288,6 @@ var (
 		"-Wno-error=deprecated",          // in external/googletest/googletest
 		// Disabling until the warning is fixed in libc++abi header files b/366180429
 		"-Wno-deprecated-dynamic-exception-spec",
-		// New warnings to be fixed after clang-r475365
-		"-Wno-error=enum-constexpr-conversion", // http://b/243964282
 		// New warnings to be fixed after clang-r522817
 		"-Wno-error=invalid-offsetof",
 		"-Wno-error=thread-safety-reference-return",
