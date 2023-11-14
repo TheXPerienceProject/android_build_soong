fbc932501e6a325015313f4e9aacfcd56d73287e
John Galt <johngaltfirstrun@gmail.com>
2023-11-14 12:30:22 -0500

    allowlists: support inline signing for U [1/2]
    For those who have used .android-certs previously, cp .android-certs/* vendor/yaap/signing/keys/
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- android/allowlists/allowlists.go
+++ android/allowlists/allowlists.go
@@ -59,6 +59,7 @@ var (
 
 		"build/bazel":                        Bp2BuildDefaultTrueRecursively,
 		"build/make/target/product/security": Bp2BuildDefaultTrue,
+		"vendor/xperience/signing/keys":           Bp2BuildDefaultTrue,
 		"build/make/tools/protos":            Bp2BuildDefaultTrue,
 		"build/make/tools/releasetools":      Bp2BuildDefaultTrue,
 		"build/make/tools/sbom":              Bp2BuildDefaultTrue,
@@ -397,6 +398,7 @@ var (
 		"build/make/core":/* recursive = */ false,
 		"build/bazel_common_rules":/* recursive = */ true,
 		"build/make/target/product/security":/* recursive = */ false,
+		"vendor/xperience/signing/keys":/* recursive = */ false,
 		// build/make/tools/signapk BUILD file is generated, so build/make/tools is not recursive.
 		"build/make/tools":/* recursive = */ false,
 		"build/pesto":/* recursive = */ true,
