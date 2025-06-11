558e0d70525bea0c6d2963222d576b2355ca632c
ralph950412 <ralph950412@gmail.com>
2025-06-11 14:31:31 +0200

    [DNM] Add ant-wireless/e2fsprogs to androidmk_allowlist
    Change-Id: Iea97ba45b0b8691c2bf7fb3b596b0aae22d24822
    Signed-off-by: klozz <carlosj@klozz.dev>

--- ui/build/androidmk_denylist.go
+++ ui/build/androidmk_denylist.go
@@ -61,6 +61,10 @@ var androidmk_denylist []string = []string{
 
 var androidmk_allowlist []string = []string{
 	"bootable/deprecated-ota/updater/Android.mk",
+        "external/ant-wireless/ant_native/Android.mk",
+        "external/ant-wireless/ant_service/Android.mk",
+        "external/e2fsprogs/Android.mk",
+	"tools/vendor/google_prebuilts/arc/Android.mk",
 }
 
 func getAllLines(ctx Context, filename string) []string {
