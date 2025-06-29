86d2d5a44a2081b4dd5d0a30c04a8bb3cf2634ad
klozz <carlosj@klozz.dev>
2025-06-28 18:13:36 -0600

    allow wlan makefiles
    Signed-off-by: klozz <carlosj@klozz.dev>

--- ui/build/androidmk_denylist.go
+++ ui/build/androidmk_denylist.go
@@ -67,6 +67,8 @@ var androidmk_allowlist []string = []string{
         "external/ant-wireless/ant_service/Android.mk",
         "external/e2fsprogs/Android.mk",
 	"tools/vendor/google_prebuilts/arc/Android.mk",
+	"hardware/qcom-caf/wlan/qcwcn/wifi_hal/Android.mk",
+	"hardware/qcom-caf/wlan/wcnss-service/Android.mk",
 }
 
 func getAllLines(ctx Context, filename string) []string {
