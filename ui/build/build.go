a703dd90a4ba15d6dc74a80a3256e5e07e99bf52
Dmitrii <bankersenator@gmail.com>
2025-11-18 02:26:19 +0000

    soong_ui: ensure temp directory exists in SetupOutDir
    The .temp directory is needed by some tools during lunch, before
    SetupTempDir is called in the main build flow. Create it early
    in SetupOutDir to avoid mktemp failures.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- ui/build/build.go
+++ ui/build/build.go
@@ -32,6 +32,7 @@ func SetupOutDir(ctx Context, config Config) {
 	ensureEmptyFileExists(ctx, filepath.Join(config.OutDir(), "Android.mk"))
 	ensureEmptyFileExists(ctx, filepath.Join(config.OutDir(), "CleanSpec.mk"))
 	ensureDirectoriesExist(ctx, config.SoongOutDir())
+	ensureDirectoriesExist(ctx, config.TempDir())
 
 	// The ninja_build file is used by our buildbots to understand that the output
 	// can be parsed as ninja output.
